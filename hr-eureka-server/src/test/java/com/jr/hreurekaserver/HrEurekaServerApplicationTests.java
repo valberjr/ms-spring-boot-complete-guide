11b8ed7bc1c7dc30914ba5b595945648b258896e
v4lb3r <valber.junior@techlead.com.br>
2021-08-30 08:21:28 -0300

    eureka server created

---
+++ hr-eureka-server/src/test/java/com/jr/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jr.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
