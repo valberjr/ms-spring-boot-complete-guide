7abd474ace0109f10b6e6ee0534f123fb676b555
v4lb3r <valber.junior@techlead.com.br>
2021-08-30 09:26:58 -0300

    hystrix fault tolerance

--- hr-payroll/src/main/java/com/jr/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/jr/hrpayroll/resources/PaymentResource.java
@@ -2,8 +2,8 @@ package com.jr.hrpayroll.resources;
 
 import com.jr.hrpayroll.entities.Payment;
 import com.jr.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +17,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Brann", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 
--- hr-worker/src/main/java/com/jr/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/jr/hrworker/resources/WorkerResource.java
@@ -2,7 +2,6 @@ package com.jr.hrworker.resources;
 
 import com.jr.hrworker.entities.Worker;
 import com.jr.hrworker.repositories.WorkerRepository;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +34,12 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        /*try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
