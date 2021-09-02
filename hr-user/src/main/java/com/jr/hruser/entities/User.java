d334d2fbcd6dcf7e0e244b12979be4b43ecbe113
v4lb3r <valber.junior@techlead.com.br>
2021-09-02 10:32:16 -0300

    userRepository, userResource, zuul config

--- hr-user/src/main/java/com/jr/hruser/entities/User.java
+++ hr-user/src/main/java/com/jr/hruser/entities/User.java
@@ -17,7 +17,10 @@ public class User implements Serializable {
     private Long id;
 
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
---
+++ hr-user/src/main/java/com/jr/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.jr.hruser.repositories;
+
+import com.jr.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+}
